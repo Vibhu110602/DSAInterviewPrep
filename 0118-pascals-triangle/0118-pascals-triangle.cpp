fe8afd623989d1f2d9dcd7910821f50335f9a276
Vaibhav Chachra <86660806+Vibhu110602@users.noreply.github.com>
2023-10-30 16:30:46 +0530

    Time: 4 ms (24.04%), Space: 7.3 MB (8.62%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    int ncr(int n,int r) {
+    int comb(int n,int r) {
         int ans=1;
-        for(int i=1;i<=r;i++){
-            ans=ans*(n-i+1)/(i);
+        for(int i=0;i<r;i++){
+            ans=ans*(n-i)/(i+1);
         }
         return ans;
     }
-    vector<vector<int>> generate(int r) {
-        vector<vector<int>> ans;
-        for(int i=0;i<r;i++){
-            vector<int> curr;
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> triangle;
+        for(int i=0;i<numRows;i++){
+            vector<int> a;
             for(int j=0;j<=i;j++){
-                curr.push_back(ncr(i,j));
+                a.push_back(comb(i,j));
             }
-            ans.push_back(curr);
+            triangle.push_back(a);
         }
-        return ans;
+        return triangle;
     }
 };
